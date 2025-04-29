22b758d00e3e1a5defb0b818bf429cb1c7629549
harshdwivediiiii <harshvardhandwivedi18@gmail.com>
2025-04-29 14:22:01 +0530

    learned arithmatic modules

---
+++ arithmatic/math.js
@@ -0,0 +1,41 @@
+// Addition
+
+
+function add(a, b) {
+    return a + b;
+    
+}
+
+console.log(add(2, 3)); 
+
+// Subtraction
+
+function subtract(a, b) {
+    return a - b;
+}   
+
+console.log(subtract(5, 3));
+
+// Multiplication
+
+function multiply(a, b) {
+    return a*b;
+}
+
+console.log(multiply(2, 3));
+
+// Division
+
+function divide(a, b) {
+    return a/b;
+}
+
+console.log(divide(6, 3));
+
+//percentage
+
+function percentage (a,b) {
+    return a/b * 100;
+}
+
+console.log(percentage(1,4));
